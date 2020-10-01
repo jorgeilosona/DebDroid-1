20b639d43d00166d902b342cee5d6fc043ef7580
WMCB-Tech <65075766+WMCB-Tech@users.noreply.github.com>
2020-10-01 13:44:34 +0800

    Create desktop-launch.sh

---
+++ Desktop-Launch/Fluxbox/desktop-launch.sh
@@ -0,0 +1,8 @@
+#### Configure Desktop Launch
+### Change xterm to startxfce4 if you have xfce4 installed
+### Don't forget to add "> /dev/null 2>&1 &" at the end
+### Otherwise. it will print terminal logs
+export DISPLAY=:1 &&
+
+### Launch Xterm
+startfluxbox > /dev/null 2>&1 &
