7d1272ea70965dcdc7adb283e6bb503bcd0dfc2e
WMCB-Tech <65075766+WMCB-Tech@users.noreply.github.com>
2020-10-01 13:53:11 +0800

    Delete desktop-launch.sh

--- Desktop-Launch/LXDE/desktop-launch.sh
+++
@@ -1,8 +0,0 @@
-#### Configure Desktop Launch
-### Change xterm to startxfce4 if you have xfce4 installed
-### Don't forget to add "> /dev/null 2>&1 &" at the end
-### Otherwise. it will print terminal logs
-export DISPLAY=:1 &&
-
-### Launch Xterm
-startlxde > /dev/null 2>&1 &
