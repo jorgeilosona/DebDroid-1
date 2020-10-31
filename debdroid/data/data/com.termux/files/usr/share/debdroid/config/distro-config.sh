ee7bf0cbc762a7bd841797cfcc6c728cc55770ad
WMCB-Tech <65075766+WMCB-Tech@users.noreply.github.com>
2020-10-31 14:04:01 +0800

    Update distro-config.sh

--- debdroid/data/data/com.termux/files/usr/share/debdroid/config/distro-config.sh
+++ debdroid/data/data/com.termux/files/usr/share/debdroid/config/distro-config.sh
@@ -17,11 +17,11 @@ apt clean
 
 ## Fix Problems
 echo ""
-echo "Exporting libgcc_s.so.1 as a walkaround for Tigervnc for pre-android 7 clients"
+echo "Exporting libgcc_s.so.1 as a walkaround for Tigervnc"
 echo "export LD_PRELOAD=$(find /usr/lib -name libgcc_s.so.1)" > /etc/profile.d/walkaround.sh
 echo "Adding /sbin path for non-root users"
 echo "export PATH=/usr/local/bin:/usr/local/sbin:/usr/local/games:/bin:/sbin:/usr/bin:/usr/sbin:/usr/games" > /etc/profile.d/sbin.sh
-echo "export LANG=C.UTF-8" > /etc/profile.d/langfix.sh
+echo "export LANG=C.UTF-8" > /etc/profile.d/langenv.sh
 
 ## Configure udisks2 as if udisks2 interrupts apt
 echo ""
@@ -31,8 +31,9 @@ dpkg --configure -a
 
 ## Setup User Accounts
 echo "Configuring User Accounts.... "
-read -p "Enter your UNIX username (adduser): " username
-adduser $username
+read -p "Enter your UNIX username (lower case): " username
+useradd -s /bin/bash -m $username
+passwd $username
 echo "Adding the user to sudoers.d file"
 echo "$username ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/$username
 echo "$username" > /etc/userinfo.rc
